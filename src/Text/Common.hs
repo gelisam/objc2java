6764f22d8b72ed82538b03f028930c4923d709f5
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-08 13:25:28 -0400

    invertible zip

--- src/Text/Common.hs
+++ src/Text/Common.hs
@@ -3,14 +3,15 @@
 module Text.Common where
 
 import Prelude (Show (..), Read (..), Eq (..), String, Integer,
-                map, (++), Maybe (..), ($), fst, not, elem, 
-                notElem, reads, Char)
+                map, (++), (&&), Maybe (..), Bool (..), ($), not, elem, 
+                notElem, reads, Char, undefined)
 
 import Control.Category (id, (.))
 
 import Control.Monad (mplus)
 
 import Data.Char (isLetter, isDigit)
+import Data.Either
 
 import qualified Text.ParserCombinators.Parsec as Parsec
 
@@ -106,3 +107,46 @@ sepBy' arg0 op arg f = chainl1' arg0 op arg (f . drop_op)
   
   drop_op :: Iso (a, ((), b)) (a, b)
   drop_op = (id *** drop_left)
+
+
+testIso :: (Eq a, Eq b) => a -> Iso a b -> b -> Bool
+testIso x iso y =   apply iso x == Just y
+               && unapply iso y == Just x
+
+fst :: Iso a a' -> Iso (a, b) (a', b)
+fst = (*** id)
+
+snd :: Iso b b' -> Iso (a, b) (a, b')
+snd = (id ***)
+
+-- | Invertible zip, fails if the lists have different lengths.
+-- 
+-- Examples:
+-- 
+-- >>> testIso [(1, '1'), (2, '2'), (3, '3')] unzip ([1,2,3], "123")
+-- True
+-- 
+-- >>> unapply unzip ([1,2,3], "123")
+-- Just [(1,'1'),(2,'2'),(3,'3')]
+-- 
+-- >>> unapply unzip ([1,2], "123")
+-- Nothing
+unzip :: Iso [(a, b)] ([a], [b])
+unzip = split . inverse listCases where
+  split :: Iso (Either () ((a, b), [(a, b)])) ([a], [b])
+  split = nils
+      ||| (conses . snd unzip)
+  
+  nils :: Iso () ([a], [b])
+  nils = (nil *** nil) . unit
+  
+  conses :: Iso ((a, b), ([a], [b])) ([a], [b])
+  conses = (cons *** cons) . redistribute
+  
+  redistribute :: Iso ((a, b), (a', b'))
+                      ((a, a'), (b, b'))
+  redistribute = associate
+               . snd ( inverse associate
+                     . fst commute
+                     . associate)
+               . inverse associate
