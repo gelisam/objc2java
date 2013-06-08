c80368431b86289bfa234f98f2bcd3890c34f380
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-08 14:06:00 -0400

    multi-arg ObjC method calls

--- src/Text/ObjC.hs
+++ src/Text/ObjC.hs
@@ -4,7 +4,7 @@ module Text.ObjC where
 
 import Prelude (Show (..), Read (..), Eq (..), String, Integer,
                 map, (++), Maybe (..), ($), fst, not, elem, 
-                notElem, reads, Char)
+                notElem, flip, reads, Char)
 
 import Control.Category (id, (.))
 
@@ -27,7 +27,8 @@ import Text.Common
 data Expr = Var String
           | StringLit String
           | MethodCall { target :: Expr
-                       , method_name :: String
+                       , method_name :: [String]
+                       , args :: [Expr]
                        }
           | FunctionCall { function_name :: String
                          , args :: [Expr]
@@ -48,9 +49,9 @@ $(defineIsomorphisms ''Expr)
 -- Just "Hello"
 -- 
 -- >>> parse expr "[[Hello alloc] init]"
--- [MethodCall {target = MethodCall {target = Var "Hello", method_name = "alloc"}, method_name = "init"}]
+-- [MethodCall {target = MethodCall {target = Var "Hello", method_name = ["alloc"], args = []}, method_name = ["init"], args = []}]
 -- 
--- >>> print expr (MethodCall (MethodCall (Var "Hello") "alloc") "init")
+-- >>> print expr (MethodCall (MethodCall (Var "Hello") ["alloc"] []) ["init"] [])
 -- Just "[[Hello alloc] init]"
 -- 
 -- >>> parse expr "NSLog(@\"Hello, World!\")"
@@ -60,14 +61,24 @@ $(defineIsomorphisms ''Expr)
 -- Just "NSLog(msg)"
 -- 
 -- >>> parse expr "NSLog ( @\"The current date and time is: %@\", [NSDate date] )"
--- [FunctionCall {function_name = "NSLog", args = [StringLit "The current date and time is: %@",MethodCall {target = Var "NSDate", method_name = "date"}]}]
+-- [FunctionCall {function_name = "NSLog", args = [StringLit "The current date and time is: %@",MethodCall {target = Var "NSDate", method_name = ["date"], args = []}]}]
 -- 
--- >>> print expr (FunctionCall "NSLog" [StringLit "The current date and time is: %@", MethodCall (Var "NSDate") "date"])
+-- >>> print expr (FunctionCall "NSLog" [StringLit "The current date and time is: %@", MethodCall (Var "NSDate") ["date"] []])
 -- Just "NSLog(@\"The current date and time is: %@\", [NSDate date])"
+-- 
+-- >>> parse expr "[myData writeToFile:@\"/tmp/log.txt\" atomically: NO]"
+-- [MethodCall {target = Var "myData", method_name = ["writeToFile","atomically"], args = [StringLit "/tmp/log.txt",Var "NO"]}]
 expr :: Syntax s => s Expr
 expr = var <$> identifier
    <|> stringLit <$> text "@" *> quoted_string
-   <|> brackets (methodCall <$> expr <*> optSpace *> identifier)
-   <|> functionCall <$> identifier <* skipSpace <*> parens (
-         sepBy expr spacedComma
-       )
+   <|> brackets (zeroary_methodCall <$> expr <*> optSpace *> identifier)
+   <|> brackets (methodCall . snd unzip <$> expr <*> optSpace *> named_args)
+   <|> functionCall <$> identifier <* skipSpace <*> positional_args
+       where
+  positional_args = parens (sepBy expr spacedComma)
+  
+  zeroary_methodCall :: Iso (Expr, String) Expr
+  zeroary_methodCall = methodCall . snd append_nil . snd singleton
+  
+  named_args :: Syntax s => s [(String, Expr)]
+  named_args = flip sepBy1 skipSpace $ identifier <* text ":" <* skipSpace <*> expr
