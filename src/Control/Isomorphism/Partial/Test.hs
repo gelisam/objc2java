ccbdcca0cc8d8f691b819a1072eb18960f070f60
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-11 23:16:24 -0400

    meta-test: a doctest for testIso
    Which is itself used to test the isomorphisms.

--- src/Control/Isomorphism/Partial/Test.hs
+++ src/Control/Isomorphism/Partial/Test.hs
@@ -2,9 +2,24 @@ module Control.Isomorphism.Partial.Test where
 
 import Prelude (Eq (..), (&&), Maybe (..), Bool)
 
+import Control.Isomorphism.Partial.Constructors (cons)
 import Control.Isomorphism.Partial (Iso, apply, unapply)
 
 
+-- | Bidirectional correctness: two tests for the price of one.
+-- 
+-- Isomorphisms are supposed to work in both direction, so you should test both:
+-- 
+-- >>> apply cons ('a',"")
+-- Just "a"
+-- 
+-- >>> unapply cons "a"
+-- Just ('a',"")
+-- 
+-- With testIso, you still test both directions, but you don't have to repeat yourself:
+-- 
+-- >>> testIso ('a',"") cons "a"
+-- True
 testIso :: (Eq a, Eq b) => a -> Iso a b -> b -> Bool
 testIso x iso y =   apply iso x == Just y
                && unapply iso y == Just x
