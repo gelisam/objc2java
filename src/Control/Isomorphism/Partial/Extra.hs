282f959b228c219297314846dd50d0811ee89856
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-11 23:34:31 -0400

    test all the things! (isomorphisms)

--- src/Control/Isomorphism/Partial/Extra.hs
+++ src/Control/Isomorphism/Partial/Extra.hs
@@ -1,3 +1,4 @@
+-- | A few useful tools when constructing isomorphisms.
 module Control.Isomorphism.Partial.Extra where
 
 import Prelude ()
@@ -5,21 +6,52 @@ import Prelude ()
 import Control.Category (id, (.))
 import Control.Isomorphism.Partial (Iso, (***), (|||),
                                     unapply, inverse, commute, associate,
-                                    unit, nil, cons, listCases)
+                                    ignore, unit, nil, cons, listCases)
 import Data.Either (Either)
 
 import Control.Isomorphism.Partial.Test (testIso)
 
+-- $setup
+-- >>> let a2b = (inverse (ignore 'b')) . ignore 'a'
 
+
+-- | Apply an isomorphism to the left part of a pair.
+-- 
+-- >>> testIso ('a', 'a') (fst a2b) ('b', 'a')
+-- True
 fst :: Iso a a' -> Iso (a, b) (a', b)
 fst = (*** id)
 
+-- | Apply an isomorphism to the right part of a pair.
+-- 
+-- >>> testIso ('a', 'a') (snd a2b) ('a', 'b')
+-- True
 snd :: Iso b b' -> Iso (a, b) (a, b')
 snd = (id ***)
 
+-- | Introduces nil in the same way as () is introduced, that is, in parallel to existing data.
+-- 
+-- The value [] is a lot like the value (), and the nil isomorphism recognizes this:
+-- 
+-- >>> testIso () nil []
+-- True
+-- 
+-- Despite the similarity, the partial-isomorphisms library only allows () to be appended for free:
+-- 
+-- >>> testIso 'a' unit ('a', ())
+-- True
+-- 
+-- The isomorphism append_nil fixes this minor oversight.
+-- 
+-- >>> testIso 'a' append_nil ('a', [])
+-- True
 append_nil :: Iso a (a, [b])
 append_nil = snd nil . unit
   
+-- | Invertible single-element list.
+-- 
+-- >>> testIso 'a' singleton "a"
+-- True
 singleton :: Iso a [a]
 singleton = cons . append_nil
 
