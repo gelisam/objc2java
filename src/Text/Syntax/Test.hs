a8a6d3069e78a519dbd6379a8ddcca615f04366c
Samuel Gélineau <gelisam+github@gmail.com>
2013-06-11 23:06:25 -0400

    meta-test: a doctest for testSyntax
    Which is itself used to test much of the rest of the code.

--- src/Text/Syntax/Test.hs
+++ src/Text/Syntax/Test.hs
@@ -5,11 +5,21 @@ import Prelude (Maybe (..), String)
 
 import Data.Maybe (isJust)
 import Control.Monad (guard)
-import Text.Syntax (Syntax)
+import Text.Syntax
 import Text.Syntax.Parser.Naive (parse)
 import Text.Syntax.Printer.Naive (print)
 
 
+-- | Correctness through whitespace-normalization: if an invertible syntax can correctly remove all the spurious space, this is a good indication that the parsing and printing phases were both successful.
+-- 
+-- >>> testSyntax (text "A" <* optSpace <*> text "B") "AB"
+-- Just "A B"
+-- 
+-- >>> testSyntax (text "A" <* skipSpace <*> text "B") "A B"
+-- Just "AB"
+-- 
+-- >>> testSyntax (text "a" <* skipSpace <*> text "b") "A B"
+-- Nothing
 testSyntax :: (forall s. Syntax s => s a) -> String -> Maybe String
 testSyntax s input = do
   [ast] <- Just (parse s input)
